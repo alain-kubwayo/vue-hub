2f32e19817c6eb7c3f27fb8489a82ce41b99a4a1
alain-kubwayo <ahkubwayo@gmail.com>
2023-05-02 09:53:27 +0200

    feat: render dynamic data

---
+++ app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    // data, functions to react to events
+    // template: '<h2>I am the template</h2>'
+    data() {
+        return {
+            title: 'The Final Empire'
+        }
+    }
+})
+
+app.mount('#app')
