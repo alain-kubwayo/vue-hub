6016be582a79818054fee2b9fe7e8c2e5bd4ffd1
alain-kubwayo <ahkubwayo@gmail.com>
2023-05-03 13:02:41 +0200

    feat: add attribute binding

--- app.js
+++ app.js
@@ -8,12 +8,13 @@ const app = Vue.createApp({
             author: 'Brandon Sanderson',
             age: 45,
             books: [
-                { title: "name of the wind", author: "patrick rothfuss" },
-                { title: "the way of the kings", author: "brandon sanderson" },
-                { title: "the final empire", author: "brandon sanderson" }
+                { title: "name of the wind", author: "patrick rothfuss", img: "./assets/1.webp" },
+                { title: "the way of the kings", author: "brandon sanderson", img: "./assets/2.jpeg" },
+                { title: "the final empire", author: "brandon sanderson", img: "./assets/3.jpeg" }
             ],
             x: 0,
-            y: 0
+            y: 0,
+            url: "https://alainkubwayo.dev"
         }
     },
     methods: {
