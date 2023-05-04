a4604a12d0fe1c64a8957e34f00cbacd12c05ab9
alain-kubwayo <ahkubwayo@gmail.com>
2023-05-04 09:32:40 +0200

    feat: add global and scoped css

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -2,6 +2,7 @@
     <div class="backdrop">
         <div class="modal">
             <p>Modal Content</p>
+            <h1>Hello</h1>
         </div>
     </div>
 </template>
@@ -22,5 +23,15 @@
         margin: 100px auto;
         background: white;
         border-radius: 10px;
+    }
+
+    .modal h1 {
+        color: green;
+        border: none;
+        padding: 0;
+    }
+
+    .modal p{
+        font-style: normal;
     }
 </style>
