6cbd022ebc5a431f6ca82bdc502fa0f09404e76c
alain-kubwayo <ahkubwayo@gmail.com>
2023-05-05 11:53:25 +0200

    feat: add composition api basics

---
+++ dojo-blog/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <h1>homepage</h1>
+    <p>{{ name }} - {{ age }}</p>
+    <button @click="handleClick">click me</button>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home',
+  setup() {
+    let name = 'alain'
+    let age = 30
+
+    const handleClick = () => {
+      console.log('you clicked me');
+    }
+
+    return { name, age, handleClick }
+  }
+}
+</script>
