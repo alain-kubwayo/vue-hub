5d4180b031390790677173c9a86899d66db86f99
alain-kubwayo <ahkubwayo@gmail.com>
2023-05-05 09:31:01 +0200

    feat: explore router links

--- ninja-jobs/src/views/Home.vue
+++ ninja-jobs/src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam magni doloremque maxime accusamus ut et sunt libero animi at atque natus distinctio consequuntur adipisci voluptatum, delectus recusandae quas totam facere.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non consequuntur hic nisi sapiente repellendus temporibus doloremque tempore unde nostrum quaerat, tenetur magnam vel harum incidunt perferendis tempora cupiditate similique! Nemo!</p>
   </div>
 </template>
 
@@ -10,9 +11,9 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
+  name: 'Home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
