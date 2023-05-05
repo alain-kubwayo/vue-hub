d32b195db946b7318c25314f598df0484bdd337d
alain-kubwayo <ahkubwayo@gmail.com>
2023-05-05 12:01:07 +0200

    feat: add template refs

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -1,25 +1,31 @@
 <template>
   <div class="home">
     <h1>homepage</h1>
-    <p>{{ name }} - {{ age }}</p>
+    <p ref="p">{{ name }} - {{ age }}</p>
     <button @click="handleClick">click me</button>
   </div>
 </template>
 
 <script>
-
+import { ref } from 'vue'
 
 export default {
   name: 'Home',
   setup() {
+    // console.log(this) // this is undefined
+    const p = ref(null)
+
     let name = 'alain'
     let age = 30
 
     const handleClick = () => {
-      console.log('you clicked me');
+      console.log('you clicked me')
+      console.log(p, p.value)
+      p.value.classList.add('test')
+      p.value.textContent = 'Hello ninjas!'
     }
 
-    return { name, age, handleClick }
+    return { name, age, handleClick, p }
   }
 }
 </script>
