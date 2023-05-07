c821aaeb46c34c1fca167af3b33de9a613f553a4
alain-kubwayo <ahkubwayo@gmail.com>
2023-05-07 07:44:05 +0200

    feat: fundamentals of reactivity in vue 3

--- composition-api/src/components/Counter.vue
+++ composition-api/src/components/Counter.vue
@@ -3,15 +3,19 @@
         <p>{{ counter }}</p>
         <p>{{ arrayOfEmojis }}</p>
         <button @click="increment">Counter</button>
+        <p>Years: {{ years }}</p>
     </div>
 </template>
 
 <script>
 
-import { onMounted, ref, watch, computed } from 'vue'
+import { onMounted, ref, watch, computed, reactive, toRef, toRefs } from 'vue'
 export default {
     props: {
-        emoji: { type: String, default: '💩' }
+        emoji: { type: String, default: '💩' },
+        a: { type: Number, default: 0 },
+        b: { type: String, default: 'Vue 3!' },
+        c: { type: Boolean, default: true }
     },
     setup(props) {
         console.log(props.emoji)
@@ -32,7 +36,23 @@ export default {
         const arrayOfEmojis = computed(() => Array.from(new Array(counter.value), () => props.emoji).join(' ')
         )
 
-        return { counter, increment, arrayOfEmojis }
+        const age = reactive({ value: 0 })
+        console.log(age)
+
+        // primitive types: to be reactive use ref instead of reactive
+        const years = ref(30)
+        console.log('years: ', years.value)
+
+        const obj = reactive({ a: 0, b: 'string' })
+        const aRef = toRef(obj, 'a') // useful when you want to pass a specific property to a function while maintaining reactivity
+        // console.log(obj, aRef)
+
+        const { a, b, c } = toRefs(props)
+        console.log(a.value)
+        console.log(b.value)
+        console.log(c.value)
+
+        return { counter, increment, arrayOfEmojis, years }
     }
 }
 </script>
