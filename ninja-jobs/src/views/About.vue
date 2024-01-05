8f1441d176110771ee9f1ed6e3c9eef2140f7bfd
alain-kubwayo <ahkubwayo@gmail.com>
2024-01-05 19:30:08 +0200

    chore: cleanup

--- ninja-jobs/src/views/About.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure fuga debitis cupiditate voluptatem necessitatibus ut voluptas corrupti quas obcaecati sequi, accusamus laboriosam molestias reprehenderit accusantium quaerat ullam repudiandae dolor consequuntur sapiente, asperiores provident. Dolorem possimus cupiditate sit corporis ratione quis sequi aliquid. Autem velit, maxime doloremque totam, impedit molestiae veniam, voluptatibus perferendis assumenda ducimus consequatur facilis corrupti laudantium excepturi non eaque delectus rem blanditiis reprehenderit aliquid quia unde numquam ipsa? Cumque placeat aspernatur explicabo vel quibusdam est odit quam exercitationem? Aut beatae, distinctio perspiciatis ut repudiandae officiis nesciunt rem iste accusantium consequatur ducimus consequuntur tempora commodi dolor nisi ad provident?</p>
-  </div>
-</template>
